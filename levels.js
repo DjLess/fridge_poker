6d9362d3d7e9dcc1da738366e66c97c7922e45ef
Felipe Castillo San Martin <56896952+DjLess@users.noreply.github.com>
2026-07-28 22:39:48 +1000

    Add files via upload

--- levels.js
+++ levels.js
@@ -1,9 +1,9 @@
 // levels.js
 export const LEVEL_CONFIGS = [
-  { week: 1, targetScore: 100, maxHands: 4, maxDiscards: 3, levelName: 'Cozy Cottage Kitchen' },
-  { week: 2, targetScore: 220, maxHands: 4, maxDiscards: 3, levelName: 'Family Diner' },
-  { week: 3, targetScore: 450, maxHands: 3, maxDiscards: 2, levelName: 'Downtown Bakery' },
-  { week: 4, targetScore: 900, maxHands: 3, maxDiscards: 2, levelName: 'Grand Country Inn' }
+  { week: 1, targetScore: 100, targetDishes: 2, maxHands: 4, maxDiscards: 3, levelName: 'Cozy Cottage Kitchen' },
+  { week: 2, targetScore: 220, targetDishes: 3, maxHands: 4, maxDiscards: 3, levelName: 'Family Diner' },
+  { week: 3, targetScore: 450, targetDishes: 3, maxHands: 3, maxDiscards: 2, levelName: 'Downtown Bakery' },
+  { week: 4, targetScore: 900, targetDishes: 4, maxHands: 3, maxDiscards: 2, levelName: 'Grand Country Inn' }
 ];
 
 export class LevelManager {
@@ -14,6 +14,7 @@ export class LevelManager {
     return {
       week: weekNumber,
       targetScore: Math.round(900 * Math.pow(1.5, weekNumber - 4)),
+      targetDishes: Math.min(5, 3 + Math.floor((weekNumber - 4) / 2)),
       maxHands: 3,
       maxDiscards: 2,
       levelName: `Kitchen W${weekNumber}`
